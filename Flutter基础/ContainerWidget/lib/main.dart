69d5ed1c5e709241e572d4b25006c3297ff81262
tTao <>
2019-08-25 22:15:35 +0800

    【Flutter】3.2 Container Widget

--- Flutter基础/ContainerWidget/lib/main.dart
+++ Flutter基础/ContainerWidget/lib/main.dart
@@ -12,10 +12,18 @@ class MyApp extends StatelessWidget {
           child: new Container(
             child:
                 new Text('Hellp Container', style: TextStyle(fontSize: 40.0)),
-            alignment: Alignment.center,
+            alignment: Alignment.topLeft,
             width: 500.0,
             height: 400.0,
-            color: Colors.lightBlue,
+            // color: Colors.lightBlue,
+            padding: const EdgeInsets.fromLTRB(10.0, 100.0, 0.0, 0.0),
+            margin: const EdgeInsets.all(20.0),
+            decoration: new BoxDecoration(
+              gradient: const LinearGradient(
+                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+              ),
+              border: Border.all(width: 5.0, color: Colors.black)
+            ),
           ),
         ),
       ),
