99b271ae85ace0997f22968a9b75f749de5a098f
QDSG <piftao1127@163.com>
2019-08-29 18:15:36 +0800

    【Flutter】4.1 - Image

---
+++ Flutter基础/Image/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/qdsg/develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/qdsg/Documents/Github/FlutterDemo/Flutter基础/Image"
+export "FLUTTER_TARGET=/Users/qdsg/Documents/Github/FlutterDemo/Flutter基础/Image/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/qdsg/develop/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
---
+++ Flutter基础/Image/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Container Widget',
+      home: Scaffold(
+        body: Center(
+          child: new Container(
+            child: new Image.network(
+              'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567074681578&di=25465c1237837d91634421e173a32b65&imgtype=0&src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-f51a87a4313286675d9750b9c5d1c42b_xl.jpg',
+              fit: BoxFit.fill,
+            ),
+            width: 300,
+            height: 300,
+            color: Colors.lightBlue,
+          ),
+        ),
+      ),
+    );
+  }
+}
