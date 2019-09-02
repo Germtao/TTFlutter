0b478d166729ce7e84ee5d3e61e25504a2d46fbb
QDSG <piftao1127@163.com>
2019-09-02 11:35:12 +0800

    【Flutter】4.2 Image

--- Flutter基础/Image/lib/main.dart
+++ Flutter基础/Image/lib/main.dart
@@ -12,7 +12,10 @@ class MyApp extends StatelessWidget {
           child: new Container(
             child: new Image.network(
               'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567074681578&di=25465c1237837d91634421e173a32b65&imgtype=0&src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-f51a87a4313286675d9750b9c5d1c42b_xl.jpg',
-              fit: BoxFit.fill,
+//              fit: BoxFit.fill,
+//              color: Colors.greenAccent,
+//              colorBlendMode: BlendMode.modulate,
+              repeat: ImageRepeat.repeat,
             ),
             width: 300,
             height: 300,
