3f7c16f76f883ed3a14cb3dbc6d87605612f46a3
QDSG <piftao1127@163.com>
2019-09-10 15:22:39 +0800

    【Flutter】5.3 动态ListView

--- Flutter基础/flutter_listview/lib/main.dart
+++ Flutter基础/flutter_listview/lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(
+  items: new List<String>.generate(1000, (i)=>'Item $i'),
+));
 
 class MyApp extends StatelessWidget {
+
+  final List<String> items;
+
+  MyApp({Key key, @required this.items}): super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,28 +18,15 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('List View Widget'),
         ),
-        body: Center(
-          child: Container(
-            height: 200.0,
-            child: MyList(),
-          ),
-        ),
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
+        )
       ),
-    );
-  }
-}
-
-class MyList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        new Container(width: 180.0, color: Colors.lightBlue,),
-        new Container(width: 180.0, color: Colors.deepOrangeAccent,),
-        new Container(width: 180.0, color: Colors.deepPurple,),
-        new Container(width: 180.0, color: Colors.lightGreenAccent,),
-      ],
     );
   }
 }
