6932d0c9c4c4c1bb3d4b5a72ce8a94484f9a1464
QDSG <piftao1127@163.com>
2019-09-16 12:32:11 +0800

    no message

--- Flutter进阶/1.底部导航栏制作和切换/flutter_bottomnavigationbar/lib/BottomNavigationWidget.dart
+++ Flutter进阶/1.底部导航栏制作和切换/flutter_bottomnavigationbar/lib/BottomNavigationWidget.dart
@@ -1,1 +1,57 @@
-import 'package:flutter/material.dart';import 'pages/home_screen.dart';import 'pages/airplay_screen.dart';import 'pages/email_screen.dart';import 'pages/profile_screen.dart';class BottomNavigationWidget extends StatefulWidget {  @override  _BottomNavigationWidgetState createState() => _BottomNavigationWidgetState();}class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {  final _bottomNavigationBarItemColor = Colors.blue;  int _currentIndex = 0; // 当前页  var pagesList = List<Widget>();  @override  void initState() {    pagesList      ..add(HomeScreen())      ..add(AirPlayScreen())      ..add(EmailScreen())      ..add(ProfileScreen());    super.initState();  }  @override  Widget build(BuildContext context) {    return Scaffold(      body: ,      bottomNavigationBar: BottomNavigationBar(        items: [          BottomNavigationBarItem(              icon: Icon(Icons.home, color: _bottomNavigationBarItemColor),              title: Text('Home', style: TextStyle(color: _bottomNavigationBarItemColor),)          ),          BottomNavigationBarItem(              icon: Icon(Icons.airplay, color: _bottomNavigationBarItemColor),              title: Text('Airplay', style: TextStyle(color: _bottomNavigationBarItemColor),)          ),          BottomNavigationBarItem(              icon: Icon(Icons.email, color: _bottomNavigationBarItemColor),              title: Text('Email', style: TextStyle(color: _bottomNavigationBarItemColor),)          ),          BottomNavigationBarItem(              icon: Icon(Icons.account_circle, color: _bottomNavigationBarItemColor),              title: Text('Profile', style: TextStyle(color: _bottomNavigationBarItemColor),)          )        ],      ),    );  }}
+import 'package:flutter/material.dart';
+import 'pages/home_screen.dart';
+import 'pages/airplay_screen.dart';
+import 'pages/email_screen.dart';
+import 'pages/profile_screen.dart';
+
+class BottomNavigationWidget extends StatefulWidget {
+  @override
+  _BottomNavigationWidgetState createState() => _BottomNavigationWidgetState();
+}
+
+class _BottomNavigationWidgetState extends State<BottomNavigationWidget> {
+
+  final _bottomNavigationBarItemColor = Colors.blue;
+
+  int _currentIndex = 0; // 当前页
+  var pagesList = List<Widget>();
+
+  @override
+  void initState() {
+
+    pagesList
+      ..add(HomeScreen())
+      ..add(AirPlayScreen())
+      ..add(EmailScreen())
+      ..add(ProfileScreen());
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: _bottomNavigationBarItemColor),
+              title: Text('Home', style: TextStyle(color: _bottomNavigationBarItemColor),)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.airplay, color: _bottomNavigationBarItemColor),
+              title: Text('Airplay', style: TextStyle(color: _bottomNavigationBarItemColor),)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.email, color: _bottomNavigationBarItemColor),
+              title: Text('Email', style: TextStyle(color: _bottomNavigationBarItemColor),)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle, color: _bottomNavigationBarItemColor),
+              title: Text('Profile', style: TextStyle(color: _bottomNavigationBarItemColor),)
+          )
+        ],
+      ),
+    );
+  }
+}
