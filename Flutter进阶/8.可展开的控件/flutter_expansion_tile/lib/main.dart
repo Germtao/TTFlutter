35dee9bf9aa1e7b3477d30bc714f1e039677f3ac
QDSG <piftao1127@163.com>
2019-09-20 17:53:55 +0800

    【Flutter】8. 可展开的控件

---
+++ Flutter进阶/8.可展开的控件/flutter_expansion_tile/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'expansion_tile_widget.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expansion Tile',
+      theme: ThemeData.dark(),
+      home: ExpansionTileWidget(),
+    );
+  }
+}
