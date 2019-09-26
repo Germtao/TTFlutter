b8ad1a27c63572a6f45bf6cabf2b117c9dd3d8a7
QDSG <piftao1127@163.com>
2019-09-26 11:39:17 +0800

    【Flutter】父Widget管理子Widget的状态

--- Flutter集合/flutter_collection/lib/basic_widget(基础控件)/state_manage.dart
+++ Flutter集合/flutter_collection/lib/basic_widget(基础控件)/state_manage.dart
@@ -21,7 +21,17 @@ class StateManager extends StatelessWidget {
               onPressed: () => Navigator.of(context).push(MaterialPageRoute(
                 builder: (context) => TapBoxA(),
               )),
-            )
+            ),
+            RaisedButton(
+              child: Text(
+                '父Widget管理子Widget的状态',
+                style: TextStyle(color: Colors.black87),
+              ),
+              color: Colors.purple[200],
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ParentWidget(),
+              )),
+            ),
           ],
         ),
       ),
@@ -66,6 +76,69 @@ class _TapBoxAState extends State<TapBoxA> {
               color: _active ? Colors.lightGreen[700] : Colors.grey[600],
             ),
           ),
+        ),
+      ),
+    );
+  }
+}
+
+// 父Widget管理子Widget的状态
+// TapboxB通过回调将其状态导出到其父组件，状态由父组件管理
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('父Widget管理子Widget的状态'),
+      ),
+      body: Center(
+        child: TapBoxB(
+          active: _active,
+          onChanged: _handleTapboxChanged,
+        ),
+      ),
+    );
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  TapBoxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
         ),
       ),
     );
