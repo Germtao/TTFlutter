16d7c6e40b466fa2958284bf3a4ebe5db9bbf83d
tTao <>
2019-10-09 22:36:09 +0800

    【Flutter】Wrap流水布局

--- Flutter集合/flutter_collection/lib/layout_widget(布局控件)/wrap_and_flox_layout.dart
+++ Flutter集合/flutter_collection/lib/layout_widget(布局控件)/wrap_and_flox_layout.dart
@@ -4,6 +4,45 @@ import 'package:flutter/material.dart';
 class WrapAndFloxLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('流式布局'),
+      ),
+      body: Wrap(
+        spacing: 8.0, // 主轴（水平）方向间距
+        runSpacing: 4.0, // 纵轴（垂直）方向间距
+        alignment: WrapAlignment.center, // 沿主轴方向居中
+        children: <Widget>[
+          Chip(
+            avatar: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Text('M'),
+            ),
+            label: Text('冒险王'),
+          ),
+          Chip(
+            avatar: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Text('P'),
+            ),
+            label: Text('苹果爸爸'),
+          ),
+          Chip(
+            avatar: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Text('A'),
+            ),
+            label: Text('阿里巴巴'),
+          ),
+          Chip(
+            avatar: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Text('H'),
+            ),
+            label: Text('华为爸爸'),
+          ),
+        ],
+      ),
+    );
   }
 }
