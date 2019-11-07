2ffd49b1c4dfcb348b14ccecf9f77478b71d5a15
QDSG <piftao1127@163.com>
2019-11-07 15:16:19 +0800

    behavior HitTestBehavior.opaque

--- Flutter集合/flutter_collection/lib/Advanced/pointer_event.dart
+++ Flutter集合/flutter_collection/lib/Advanced/pointer_event.dart
@@ -15,19 +15,18 @@ class _PointerEventTestRouteState extends State<PointerEventTestRoute> {
       child: Scaffold(
         appBar: AppBar(title: Text('原始指针事件处理')),
         body: Listener(
-          child: Container(
-            alignment: Alignment.center,
-            color: Colors.blue,
-            width: 300.0,
-            height: 150.0,
-            child: Text(
-              _event?.toString() ?? "",
-              style: TextStyle(color: Colors.white),
+          child: ConstrainedBox(
+            constraints: BoxConstraints.tight(Size(300.0, 150.0)),
+            child: Center(
+              child: Text(
+                'Box A',
+                style:
+                    TextStyle(color: Colors.white, backgroundColor: Colors.red),
+              ),
             ),
           ),
-          onPointerDown: (event) => setState(() => _event = event),
-          onPointerMove: (event) => setState(() => _event = event),
-          onPointerUp: (event) => setState(() => _event = event),
+          behavior: HitTestBehavior.opaque,
+          onPointerDown: (event) => print('down Box A'),
         ),
       ),
     );
