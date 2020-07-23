1b3721c3eae4b00b40326cc253e3b40b9d9de48e
QDSG <piftao1127@163.com>
2020-07-23 10:50:12 +0800

    帖子栏点赞组件

---
+++ flutter_pro_cli_test/lib/widgets/home_page/article_like_bar.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:flutter_pro_cli_test/styles/text_styles.dart';
+import 'package:flutter_pro_cli_test/model/like_num_model.dart';
+
+/// 帖子文章的赞组件
+///
+/// 包括点赞组件 icon ，以及组件点击效果
+/// 需要外部参数[likeNum],点赞数量
+class ArticleLikeBar extends StatelessWidget {
+  /// 帖子 id
+  final String articleId;
+
+  /// 点赞数
+  final int likeNum;
+
+  const ArticleLikeBar({Key key, this.articleId, this.likeNum})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final likeNumModel = Provider.of<LikeNumModel>(context);
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        FlatButton(
+          child: Row(
+            children: [
+              Icon(Icons.thumb_up, color: Colors.grey, size: 18.0),
+              Padding(padding: EdgeInsets.only(left: 10)),
+              Text(
+                '${likeNumModel.getLikeNum(articleId, likeNum)}',
+                style: TextStyles.commonStyle(),
+              )
+            ],
+          ),
+          onPressed: () => likeNumModel.like(articleId),
+        )
+      ],
+    );
+  }
+}
