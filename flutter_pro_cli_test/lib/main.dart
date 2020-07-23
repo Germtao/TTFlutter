8bb44e038e951db68e0c5bcdbe7f441ea11b8e32
QDSG <piftao1127@163.com>
2020-07-23 15:34:19 +0800

    main

--- flutter_pro_cli_test/lib/main.dart
+++ flutter_pro_cli_test/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'package:flutter_pro_cli_test/model/like_num_model.dart';
 import 'package:flutter_pro_cli_test/pages/entrance_bottom_bar.dart';
 // import 'package:flutter_pro_cli_test/pages/entrance.dart';
 import 'package:flutter_pro_cli_test/router.dart';
@@ -10,17 +12,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final likeNumModel = LikeNumModel();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Two You', // App 名字
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue, // App 主题
+    return Provider<Map<String, int>>.value(
+      value: {},
+      child: ChangeNotifierProvider.value(
+        value: likeNumModel,
+        child: MaterialApp(
+          title: 'Two You', // App 名字
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue, // App 主题
+          ),
+          routes: Router().registerRouter(),
+          home: Entrance(),
+        ),
       ),
-      routes: Router().registerRouter(),
-      home: Entrance(),
     );
   }
 }
