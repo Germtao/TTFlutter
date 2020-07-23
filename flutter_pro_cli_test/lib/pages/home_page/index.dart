1b62c401fc541134453a0f42408ab23450bd3420
QDSG <piftao1127@163.com>
2020-07-23 14:48:23 +0800

    首页

--- flutter_pro_cli_test/lib/pages/home_page/index.dart
+++ flutter_pro_cli_test/lib/pages/home_page/index.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
-class HomePageIndex extends StatelessWidget {
-  /// 构造函数
-  const HomePageIndex({Key key}) : super(key: key);
+import 'package:flutter_pro_cli_test/api/content/index.dart';
+import 'package:flutter_pro_cli_test/util/struct/content_detail.dart';
+import 'package:flutter_pro_cli_test/widgets/home_page/article_card.dart';
+
+/// 首页
+class HomePageIndex extends StatefulWidget {
+  HomePageIndex({Key key}) : super(key: key);
+
+  @override
+  _HomePageIndexState createState() => _HomePageIndexState();
+}
+
+/// 首页具体的state类
+class _HomePageIndexState extends State<HomePageIndex> {
+  /// 首页推荐帖子列表
+  List<StructContentDetail> contentList;
+
+  @override
+  void initState() {
+    super.initState();
+    // 拉取推荐内容
+    setState(() {
+      contentList = ApiContentIndex().getRecommendList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Text('I am home page.');
+    return ListView.separated(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemBuilder: (context, position) {
+        return ArticleCard(articleInfo: this.contentList[position]);
+      },
+      separatorBuilder: (context, index) {
+        return Divider(
+          height: .5,
+          color: Color(0xFFDDDDDD),
+        );
+      },
+      itemCount: contentList.length,
+    );
   }
 }
