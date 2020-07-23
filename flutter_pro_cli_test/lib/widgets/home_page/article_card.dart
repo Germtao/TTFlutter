6a0346cbf1214b3b0741d7ae23c018ad3e1f64d2
QDSG <piftao1127@163.com>
2020-07-23 10:59:24 +0800

    帖子所有信息展示组件

---
+++ flutter_pro_cli_test/lib/widgets/home_page/article_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_pro_cli_test/router.dart';
+import 'package:flutter_pro_cli_test/util/struct/content_detail.dart';
+
+import 'package:flutter_pro_cli_test/widgets/home_page/article_summary.dart';
+import 'package:flutter_pro_cli_test/widgets/home_page/article_like_bar.dart';
+import 'package:flutter_pro_cli_test/widgets/home_page/article_bottom_bar.dart';
+
+/// 此为帖子描述类，包括了帖子UI中的所有元素
+class ArticleCard extends StatelessWidget {
+  /// 传入的帖子信息
+  final StructContentDetail articleInfo;
+
+  const ArticleCard({Key key, this.articleInfo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(8),
+      child: FlatButton(
+        onPressed: () {},
+        child: Column(
+          children: [
+            ArticleSummary(
+              title: articleInfo.title,
+              summary: articleInfo.summary,
+              articleImage: articleInfo.articleImage,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 9,
+                  child: ArticleBottomBar(
+                    nickname: articleInfo.userInfo.nickname,
+                    avatar: articleInfo.userInfo.avatar,
+                    commentNum: articleInfo.commentNum,
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: ArticleLikeBar(
+                    articleId: articleInfo.id,
+                    likeNum: articleInfo.likeNum,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
