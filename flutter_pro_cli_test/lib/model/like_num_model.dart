bbf099a4e357d74e4ce5a0c5626667315e79fc18
QDSG <piftao1127@163.com>
2020-07-23 10:08:35 +0800

    Model - 应用 Provider 来实现状态管理

---
+++ flutter_pro_cli_test/lib/model/like_num_model.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+/// name 状态管理模块
+class LikeNumModel with ChangeNotifier {
+  /// 声明私有变量
+  Map<String, int> _likeInfo;
+
+  /// 设置初始 like num
+  void setLikeNum(String articleId, int likeNum) {
+    if (_likeInfo == null) {
+      _likeInfo = {};
+    }
+
+    if (articleId == null) return;
+
+    _likeInfo[articleId] = likeNum;
+  }
+
+  /// 设置 get 方法
+  int getLikeNum(String articleId, [int likeNum = 0]) {
+    if (_likeInfo == null) _likeInfo = {};
+
+    if (articleId == null) return likeNum;
+
+    if (_likeInfo[articleId] == null) {
+      _likeInfo[articleId] = likeNum;
+    }
+
+    return _likeInfo[articleId];
+  }
+
+  /// 点赞某个帖子
+  void like(String articleId) {
+    if (_likeInfo == null || articleId == null) {
+      _likeInfo = {};
+    }
+
+    if (_likeInfo == null || _likeInfo[articleId] == null) {
+      _likeInfo[articleId] = 0;
+    }
+
+    _likeInfo[articleId] = _likeInfo[articleId] + 1;
+
+    notifyListeners();
+  }
+}
