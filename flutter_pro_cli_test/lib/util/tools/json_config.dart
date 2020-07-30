e7413ed292f9aebc5dd45485c4b38cd78cfb09fa
QDSG <piftao1127@163.com>
2020-07-30 16:11:08 +0800

    服务通信：Flutter 中常见的网络协议

--- flutter_pro_cli_test/lib/util/tools/json_config.dart
+++ flutter_pro_cli_test/lib/util/tools/json_config.dart
@@ -3,27 +3,23 @@ import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 
 /// 缓存配置信息，避免多次读取文件
-const Map cache = {};
+const Map<String, Map<String, dynamic>> cache = {};
 
 /// 获取 json 配置文件
 ///
 /// 在使用该方法前，需要在 pubspec.yaml 中增加 assets 的配置引入
 class JsonConfig {
   /// 读取 json 配置文件，并解析返回
-  static Future getConfig(String fileName) async {
-    if (cache[fileName] != null) {
-      return cache[fileName];
-    }
+  static Future<Map<String, dynamic>> getConfig(String fileName) async {
 
-    final jsonString = await rootBundle.loadString('assets/json/${fileName}.dart');
+    final jsonString = await rootBundle.loadString('assets/json/${fileName}.json');
 
     try {
-      var jsonRet = json.decode(jsonString);
-      cache[fileName] = jsonRet;
+      Map<String, dynamic> jsonRet = json.decode(jsonString) as Map<String, dynamic>;
       return jsonRet;
     } catch (e) {
-      // @todo add report log
-      return false;
+      print(e);
+      return null;
     }
   }
 
