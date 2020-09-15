7e9576d129ef252e7977968a658e919ec0fe73db
QDSG <piftao1127@163.com>
2020-09-15 16:38:46 +0800

    在flutter中截屏

--- Flutter集合/flutter_collection/lib/官方控件系列/功能/在flutter中截屏/widget_to_image_demo.dart
+++ Flutter集合/flutter_collection/lib/官方控件系列/功能/在flutter中截屏/widget_to_image_demo.dart
@@ -4,6 +4,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:dio/dio.dart';
 
 /// 实现原理：
 ///
@@ -26,7 +27,30 @@ class WidgetToImageDemo extends StatefulWidget {
 
 class _WidgetToImageDemoState extends State<WidgetToImageDemo> {
   final GlobalKey _key = GlobalKey();
-  List<Uint8List> images = List();
+  List<String> images;
+
+  ScrollController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    images = List();
+    _controller = ScrollController();
+
+    fetchTen();
+
+    _controller.addListener(() {
+      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+        fetchTen();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,14 +60,25 @@ class _WidgetToImageDemoState extends State<WidgetToImageDemo> {
         appBar: AppBar(
           title: Text('在Flutter中截屏'),
         ),
-        body: ListView.builder(
-          itemCount: images.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Image.memory(
-              images[index],
-              fit: BoxFit.cover,
-            );
+        body: RefreshIndicator(
+          onRefresh: () async {
+            await Future.delayed(const Duration(seconds: 1));
+            images.clear();
+            fetchTen();
           },
+          child: ListView.builder(
+            controller: _controller,
+            itemCount: images.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                constraints: BoxConstraints.tightFor(height: 150.0),
+                child: Image.network(
+                  images[index],
+                  fit: BoxFit.cover,
+                ),
+              );
+            },
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: _capturePng,
@@ -55,20 +90,55 @@ class _WidgetToImageDemoState extends State<WidgetToImageDemo> {
 
   Future<Uint8List> _capturePng() async {
     try {
+      print('inside');
       RenderRepaintBoundary boundary = _key.currentContext.findRenderObject();
       ui.Image image = await boundary.toImage(pixelRatio: 3.0);
       ByteData byteData = await image.toByteData(format: ui.ImageByteFormat.png);
       Uint8List pngBytes = byteData.buffer.asUint8List();
-      images.add(pngBytes);
       String bs64 = base64Encode(pngBytes);
       print('unit8List = $pngBytes');
       print('base64 = $bs64');
-      setState(() {});
+      setState(() {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Show Image'),
+            ),
+            body: Container(
+              child: ListView(
+                children: [
+                  Image.memory(pngBytes, fit: BoxFit.cover),
+                ],
+              ),
+            ),
+          );
+        }));
+      });
 
       return pngBytes;
     } catch (e) {
       print(e);
     }
     return null;
+  }
+
+  fetch() async {
+    final response = await Dio().get('http://dog.ceo/api/breeds/image/random');
+
+    final model = json.decode(response.toString());
+
+    if (model['status'] == 'success') {
+      setState(() {
+        images.add(model['message']);
+      });
+    } else {
+      throw Exception('Failed to load images');
+    }
+  }
+
+  fetchTen() {
+    for (int i = 0; i < 10; i++) {
+      fetch();
+    }
   }
 }
