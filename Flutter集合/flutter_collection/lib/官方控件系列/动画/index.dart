9b226716824de22ef6f913842c1be69183565a91
QDSG <piftao1127@163.com>
2020-09-16 17:41:43 +0800

    基本动画样例

--- Flutter集合/flutter_collection/lib/官方控件系列/动画/index.dart
+++ Flutter集合/flutter_collection/lib/官方控件系列/动画/index.dart
@@ -1,7 +1,7 @@
-export './基础动画/basic_animation_demo.dart';
-export './延迟动画/delayed_animation_demo.dart';
-export './父子动画/parent_animation_demo.dart';
-export './父子动画/another_parent_animation_demo.dart';
-export './转换动画/transforming_animation_demo.dart';
-export './价值转换动画/value_change_animation_demo.dart';
-export './动画隐藏显示小部件/hidden_widget_animation_demo.dart';
+export './基本动画样例/基础动画/basic_animation_demo.dart';
+export './基本动画样例/延迟动画/delayed_animation_demo.dart';
+export './基本动画样例/父子动画/parent_animation_demo.dart';
+export './基本动画样例/父子动画/another_parent_animation_demo.dart';
+export './基本动画样例/转换动画/transforming_animation_demo.dart';
+export './基本动画样例/价值转换动画/value_change_animation_demo.dart';
+export './基本动画样例/动画隐藏显示小部件/hidden_widget_animation_demo.dart';
