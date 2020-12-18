49a20f71eb028d994cf48bdacb84f46ff45389bd
QDSG <piftao1127@163.com>
2020-12-18 18:06:47 +0800

    main tab bar page

---
+++ flutter_github_app/lib/page/main/main_tab_bar_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_github_app/common/style/style.dart';
+
+class MainTabBarPage extends StatefulWidget {
+  MainTabBarPage({Key key}) : super(key: key);
+
+  @override
+  _MainTabBarPageState createState() => _MainTabBarPageState();
+}
+
+class _MainTabBarPageState extends State<MainTabBarPage> {
+  final ScrollController scrollController = ScrollController();
+
+  String beStaredCount = '---';
+
+  Color notifyColor = TTColors.subTextColor;
+
+  // Store
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
