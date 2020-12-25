7d7019983693796b58ff3e2f8e750ea19f8aad0b
QDSG <piftao1127@163.com>
2020-12-25 18:01:30 +0800

    net

---
+++ flutter_github_app/lib/common/net/api.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+
+/// http请求
+class HttpManager {
+  static const CONTENT_TYPE_JSON = 'application/json';
+  static const CONTENT_TYPE_FORM = 'application/x-www-form-urlencoded';
+
+  Dio _dio = Dio(); // 使用默认配置
+}
