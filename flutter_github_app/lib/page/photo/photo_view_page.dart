70f47fa132782c3fbac29b22da6e063a6c79758a
QDSG <piftao1127@163.com>
2020-12-25 18:00:43 +0800

    photo view page

---
+++ flutter_github_app/lib/page/photo/photo_view_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PhotoViewPage extends StatelessWidget {
+  static const className = 'PhotoViewPage';
+
+  PhotoViewPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
