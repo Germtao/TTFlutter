0166d82e1a14abf4c5c0f478aa7b9d9b6425d90c
QDSG <piftao1127@163.com>
2020-12-25 14:00:14 +0800

    第三方库依赖

--- flutter_github_app/lib/common/utils/navigator_utils.dart
+++ flutter_github_app/lib/common/utils/navigator_utils.dart
@@ -1,7 +1,15 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
+import 'package:flutter_github_app/common/router/animation_route.dart';
+
 import 'package:flutter_github_app/page/home/home_page.dart';
+import 'package:flutter_github_app/page/photo/photo_view_page.dart';
+import 'package:flutter_github_app/page/user/person_detail_page.dart';
+import 'package:flutter_github_app/page/repos/repos_detail_page.dart';
+import 'package:flutter_github_app/page/webview/tt_webview.dart';
 
 class NavigatorUtils {
   /// 替换
@@ -17,6 +25,34 @@ class NavigatorUtils {
   /// 主页
   static pushHome(BuildContext context) {
     Navigator.pushReplacementNamed(context, HomePage.className);
+  }
+
+  /// 图片预览
+  static pushPhotoViewPage(BuildContext context, String urlStr) {
+    Navigator.pushNamed(context, PhotoViewPage.className, arguments: urlStr);
+  }
+
+  /// 个人详情
+  static pushPersonDetailPage(BuildContext context, String username) {
+    NavigatorRouter(context, PersonDetailPage(username));
+  }
+
+  /// 仓库详情
+  static Future pushReposDetailPage(BuildContext context, String username, String reposname) {
+    return Navigator.push(
+      context,
+      SizeRoute(
+        widget: pageContainer(
+          RepositoryDetailPage(username, reposname),
+          context,
+        ),
+      ),
+    );
+  }
+
+  /// 全屏 web 页面
+  static Future pushTTWebView(BuildContext context, String urlStr, String title) {
+    return NavigatorRouter(context, TTWebView(urlStr, title));
   }
 
   /// 公共打开方式
