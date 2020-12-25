4764a31ea5e73fff1a2e8cb232b498d14989879b
QDSG <piftao1127@163.com>
2020-12-25 18:00:20 +0800

    webview

---
+++ flutter_github_app/lib/page/webview/tt_webview.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// webview
+class TTWebView extends StatefulWidget {
+  final String urlStr;
+  final String title;
+
+  TTWebView(this.urlStr, this.title);
+
+  @override
+  _TTWebViewState createState() => _TTWebViewState();
+}
+
+class _TTWebViewState extends State<TTWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
