fcbeb7def2027bccded0ee7d3329cfac8f9fe72b
QDSG <piftao1127@163.com>
2020-12-30 16:45:04 +0800

    数据库表

---
+++ flutter_github_app/lib/db/sql_provider.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:meta/meta.dart';
+
+import 'sql_manager.dart';
+
+/// 数据库表
+/// 基类
+abstract class BaseDBProvider {
+  bool isTableExits = false;
+
+  tableSqlString();
+
+  tableName();
+
+  tableBaseString(String name, String columnId) {
+    return '''
+        create table $name (
+        $columnId integer primary key autoincrement,
+    ''';
+  }
+
+  Future<Database> getDatabase() async {
+    return await open();
+  }
+
+  @mustCallSuper
+  prepare(name, String createSql) async {
+    isTableExits = await SqlManager.isTableExits(name);
+    if (!isTableExits) {
+      Database db = await SqlManager.getCurrentDatabase();
+      return await db.execute(createSql);
+    }
+  }
+
+  @mustCallSuper
+  open() async {
+    if (!isTableExits) {
+      await prepare(tableName(), tableSqlString());
+    }
+    return await SqlManager.getCurrentDatabase();
+  }
+}
