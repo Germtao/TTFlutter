84ab69e2486eefad9bdc483253ecd334b57f0933
QDSG <piftao1127@163.com>
2021-01-25 17:55:29 +0800

    client_id、client_secret

--- flutter_github_app/lib/common/config/ignore_config.dart
+++ flutter_github_app/lib/common/config/ignore_config.dart
@@ -1,5 +1,5 @@
 class NetConfig {
-  static const CLIENT_ID = '';
+  static const CLIENT_ID = '0117d83055c10f2fd59e';
 
-  static const CLIENT_SECRET = '';
+  static const CLIENT_SECRET = 'f6544b3158d603361f1b45d68430c8c5c4c27c3d';
 }
