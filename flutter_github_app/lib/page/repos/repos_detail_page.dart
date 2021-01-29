fa8413a000a01a4420bff574af4f2f58c2add172
QDSG <piftao1127@163.com>
2021-01-29 17:49:24 +0800

    仓库详情页面

--- flutter_github_app/lib/page/repos/repos_detail_page.dart
+++ flutter_github_app/lib/page/repos/repos_detail_page.dart
@@ -3,20 +3,30 @@ import 'package:flutter/material.dart';
 /// 仓库详情
 class RepositoryDetailPage extends StatefulWidget {
   /// 用户名
-  final String username;
+  final String userName;
 
   /// 仓库名
-  final String reposname;
+  final String reposName;
 
-  RepositoryDetailPage(this.username, this.reposname);
+  RepositoryDetailPage(this.userName, this.reposName);
 
   @override
   _RepositoryDetailPageState createState() => _RepositoryDetailPageState();
 }
 
-class _RepositoryDetailPageState extends State<RepositoryDetailPage> {
+class _RepositoryDetailPageState extends State<RepositoryDetailPage> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+}
+
+/// 底部状态实体
+class BottomStatusModel {
+  final String watchText;
+  final String starText;
+  final IconData watchIcon;
+  final IconData starIcon;
+
+  BottomStatusModel(this.watchText, this.starText, this.watchIcon, this.starIcon);
 }
